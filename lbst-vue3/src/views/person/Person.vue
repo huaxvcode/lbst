c49ce0839c8d7536a1108419e0aabe00a55c68aa
huaxvcode <huaxvcode@yeah.net>
2024-08-17 23:38:20 +0800

    message

--- lbst-vue3/src/views/person/Person.vue
+++ lbst-vue3/src/views/person/Person.vue
@@ -1,47 +1,118 @@
 <template>
-  <div class="person-div">
-    <div class="person-header">
-      <el-avatar :src="headImgUrl" />
-    </div>
-    <div class="person-info">
-      <div class="person-card">
-        <p>id</p>
-        <p>昵称</p>
-        <p>出生年份</p>
-        <p>学历</p>
-      </div>
-    </div>
+  <div class="person">
+    <el-form :model="form" label-width="auto" style="max-width: 600px">
+      <el-form-item label="头像">
+        <el-avatar :src="form.headImgUrl" v-if="form.headImgUrl" />
+        <el-avatar :icon="UserFilled" v-else />
+      </el-form-item>
+      <el-form-item label="UID">
+        <el-input
+          v-model="form.uid"
+          style="width: 240px"
+          :placeholder="data.uid"
+          :disabled="!canModify"
+        />
+      </el-form-item>
+      <el-form-item label="昵称">
+        <el-input
+          v-model="form.nickName"
+          style="width: 240px"
+          :placeholder="
+            data.nickName && data.nickName != 'unnamed'
+              ? data.nickName
+              : '未录入'
+          "
+          :disabled="!canModify"
+        />
+      </el-form-item>
+      <el-form-item label="出生年份">
+        <el-input
+          v-model="data.birthYear"
+          style="width: 240px"
+          :placeholder="data.yearOfBirth ? data.yearOfBirth : '未录入'"
+          :disabled="!canModify"
+        />
+      </el-form-item>
+      <el-form-item label="学历">
+        <el-select
+          v-model="data.education"
+          :placeholder="data.education ? data.education : Select"
+          size="large"
+          style="width: 240px"
+          :disabled="!canModify"
+        >
+          <el-option
+            v-for="(item, index) in edList"
+            :key="index"
+            :label="item"
+            :value="index"
+          />
+        </el-select>
+      </el-form-item>
+    </el-form>
   </div>
 </template>
 
 <script setup>
-import { ref } from "vue";
-import { flushHeadImg } from "../../api/personApi";
-const headImgUrl = ref(null);
-flushHeadImg().then((res) => (headImgUrl.value = res));
+import { onMounted, reactive, ref } from "vue";
+import { flushHeadImg, getUserInfo } from "../../api/personApi";
+import { UserFilled } from "@element-plus/icons-vue";
+
+const form = reactive({
+  headImgUrl: "",
+  uid: "",
+  nickName: "",
+  birthYear: "",
+  education: "",
+});
+
+const data = reactive({
+  uid: "",
+  nickName: "",
+  yearOfBirth: "",
+  education: "",
+});
+
+const edList = ["小学", "初中", "高中", "大学", "硕士", "博士", "博士后"];
+
+const initForm = () => {
+  flushHeadImg().then((res) => (form.headImgUrl = res));
+  data.uid = localStorage.getItem("userId");
+
+  getUserInfo(data.uid)
+    .then((resp) => resp.data)
+    .then((res) => {
+      let rdata = res.data;
+      data.nickName = rdata.nickName;
+      data.yearOfBirth = rdata.yearOfBirth;
+      data.education = rdata.education;
+    });
+};
+
+initForm();
+
+const canModify = ref(false);
+
+const clearForm = () => {
+  form.uid = "";
+  form.nickName = "";
+  form.birthYear = "";
+  form.education = "";
+};
+
+const modifyData = () => {
+  data.nickName = form.nickName;
+  data.yearOfBirth = form.birthYear;
+  data.education = form.education;
+};
+
+setInterval(() => {
+  console.log(form);
+}, 1000 * 5);
 </script>
 
 <style scoped>
-.person-div {
-  @apply w-full h-full p-1;
-  position: relative;
-}
-
-.person-header {
-  @apply w-50 h-50 absolute;
-}
-
-.person-info {
-  @apply w-full h-full border-solid border-50 border-transparent;
-
-  background-clip: padding-box; /* 背景只覆盖到内边距区域 */
-}
-
-.person-card {
-  @apply w-full h-full rounded-2xl shadow-xl bg-light-50;
-}
-
-::v-deep .el-avatar {
-  @apply w-full h-full;
+.person {
+  @apply w-full h-full bg-white shadow-lg rounded-lg;
 }
 </style>

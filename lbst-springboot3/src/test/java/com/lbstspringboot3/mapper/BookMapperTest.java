6e957810a796bf06998d9c34e8885a3b245f5bb9
huaxvcode <huaxvcode@yeah.net>
2024-08-30 09:01:13 +0800

    message

--- lbst-springboot3/src/test/java/com/lbstspringboot3/mapper/BookMapperTest.java
+++ lbst-springboot3/src/test/java/com/lbstspringboot3/mapper/BookMapperTest.java
@@ -1,8 +1,5 @@
 package com.lbstspringboot3.mapper;
 
-import com.baomidou.mybatisplus.core.metadata.IPage;
-import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
-import java.util.Map;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -10,13 +7,12 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 public class BookMapperTest {
 
-  @Autowired BookMapper bookMapper;
+	@Autowired
+	BookMapper bookMapper;
 
-  @Test
-  public void getBookList() {
-    IPage<Map<String, Object>> page = new Page<>(1, 10);
-    var res = bookMapper.getBookList(page, null);
-    System.out.println(res.getTotal());
-    System.out.println(res.getRecords());
-  }
+	@Test
+	public void getBookList() {
+
+
+	}
 }
--- lbst-vue3/vite.config.js
+++ lbst-vue3/vite.config.js
@@ -1,25 +1,25 @@
-import {defineConfig} from 'vite'
-import vue from '@vitejs/plugin-vue'
-import AutoImport from 'unplugin-auto-import/vite'
-import Components from 'unplugin-vue-components/vite'
-import {ElementPlusResolver} from 'unplugin-vue-components/resolvers'
-import WindiCSS from 'vite-plugin-windicss'
+import {defineConfig} from "vite";
+import vue from "@vitejs/plugin-vue";
+import AutoImport from "unplugin-auto-import/vite";
+import Components from "unplugin-vue-components/vite";
+import {ElementPlusResolver} from "unplugin-vue-components/resolvers";
+import WindiCSS from "vite-plugin-windicss";
 
 export default defineConfig({
-                                // ...
-                                plugins: [
-                                    vue(),
-                                    // ...
-                                    AutoImport({
-                                                   resolvers: [ElementPlusResolver()],
-                                                   imports: ['vue'],
-                                               }),
-                                    Components({
-                                                   resolvers: [ElementPlusResolver()],
-                                               }),
-                                    WindiCSS(),
-                                ],
-                                server: {
-                                    port: 8091,
-                                }
-                            })
+    // ...
+    plugins: [
+        vue(),
+        // ...
+        AutoImport({
+            resolvers: [ElementPlusResolver()],
+            imports: ["vue"],
+        }),
+        Components({
+            resolvers: [ElementPlusResolver()],
+        }),
+        WindiCSS(),
+    ],
+    server: {
+        port: 8092,
+    },
+});
